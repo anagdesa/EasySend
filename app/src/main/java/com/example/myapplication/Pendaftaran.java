751b64275b1b45ef17b4eab71f218ebd9c351bba
Gramandha Wega Intyanto <wega.guitar@gmail.com>
2019-10-24 07:23:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Pendaftaran.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pendaftaran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pendaftaran);
+    }
+}
